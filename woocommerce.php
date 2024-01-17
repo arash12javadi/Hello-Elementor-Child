04415215f90e7fa737e86cd9c77969e094767506
Arash Javadi <arash12javadi@gmail.com>
2024-01-17 18:42:26 +0000

    Update woocommerce.php

--- woocommerce.php
+++ woocommerce.php
@@ -6,13 +6,9 @@
 
 <?php get_header(); ?>
 
-
 <div class="content mt-5">
-
     <div class="container-fluid px-5">
-
         <div class="row">
-
 
             <div class="col-xl-2 col-lg-3 AJDWP-side-bar rounded shadow pt-4 pb-4 mb-5 mb-lg-0 ">
                 <div class="sticky-top">
@@ -20,26 +16,18 @@
                 </div>
             </div>
 
-
-
-
 			<div class="col-xl-10 col-lg-9">
-
 				<section id="post-section" class="post-section">
 					<div class="container-fluid">
 						<div class="row">
-
-						<?php woocommerce_content(); ?>
-							
-
+							<?php woocommerce_content(); ?>
 						</div>
 					</div>
 				</section>
-
 			</div>
+			
 		</div>
 	</div>
 </div>
-
 
 <?php get_footer(); ?>
