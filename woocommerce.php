80e4645e64871511656605129ba0c56e7593fc38
Arash Javadi <arash12javadi@gmail.com>
2024-01-17 18:43:24 +0000

    Update woocommerce.php

--- woocommerce.php
+++ woocommerce.php
@@ -10,22 +10,22 @@
     <div class="container-fluid px-5">
         <div class="row">
 
-            <div class="col-xl-2 col-lg-3 AJDWP-side-bar rounded shadow pt-4 pb-4 mb-5 mb-lg-0 ">
-                <div class="sticky-top">
-                    <?php dynamic_sidebar('AJDWP-Shop-sidebar'); ; ?>
-                </div>
-            </div>
-
-			<div class="col-xl-10 col-lg-9">
-				<section id="post-section" class="post-section">
-					<div class="container-fluid">
-						<div class="row">
-							<?php woocommerce_content(); ?>
-						</div>
-					</div>
-				</section>
+		<div class="col-xl-2 col-lg-3 AJDWP-side-bar rounded shadow pt-4 pb-4 mb-5 mb-lg-0 ">
+			<div class="sticky-top">
+				<?php dynamic_sidebar('AJDWP-Shop-sidebar'); ; ?>
 			</div>
-			
+		</div>
+
+		<div class="col-xl-10 col-lg-9">
+			<section id="post-section" class="post-section">
+				<div class="container-fluid">
+					<div class="row">
+						<?php woocommerce_content(); ?>
+					</div>
+				</div>
+			</section>
+		</div>
+
 		</div>
 	</div>
 </div>
