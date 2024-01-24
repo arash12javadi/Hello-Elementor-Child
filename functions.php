d784c290865ace1050aa921f2435ac4b5afa10b6
Arash Javadi <arash12javadi@gmail.com>
2024-01-24 00:48:42 +0000

    Update functions.php

--- functions.php
+++ functions.php
@@ -20,20 +20,18 @@ function your_theme_enqueue_styles() {
 
 add_action('wp_enqueue_scripts', 'your_theme_enqueue_styles');
 
-//_____________________________________ Theme update from github _____________________________________//
+//__________________________________________________________________________//
+//			Theme Update From Github Repo
+//__________________________________________________________________________//
 
 // Automatic theme updates from the GitHub repository
 add_filter('pre_set_site_transient_update_themes', 'automatic_GitHub_updates', 100, 1);
 function automatic_GitHub_updates($data) {
-    // Theme information
-    $theme   = get_stylesheet(); // Folder name of the current theme
-    $current = wp_get_theme()->get('Version'); // Get the version of the current theme
-    // GitHub information
-    $user = 'arash12javadi'; // The GitHub username hosting the repository
-    $repo = 'AJDWP-Child-Theme'; // Repository name as it appears in the URL
-    // Get the latest release tag from the repository. The User-Agent header must be sent, as per
-    // GitHub's API documentation: https://developer.github.com/v3/#user-agent-required
-    $file = @json_decode(@file_get_contents('https://api.github.com/repos/'.$user.'/'.$repo.'/releases/latest', false,
+    $theme      = get_stylesheet(); // Folder name of the current theme
+    $current    = wp_get_theme()->get('Version'); // Get the version of the current theme
+    $user       = 'arash12javadi'; // The GitHub username hosting the repository
+    $repo       = 'AJDWP-Child-Theme'; // Repository name as it appears in the URL
+    $file       = @json_decode(@file_get_contents('https://api.github.com/repos/'.$user.'/'.$repo.'/releases/latest', false,
         stream_context_create(['http' => ['header' => "User-Agent: ".$user."\r\n"]])
     ));
     $update = filter_var($file->tag_name, FILTER_SANITIZE_NUMBER_FLOAT, FILTER_FLAG_ALLOW_FRACTION);
@@ -47,7 +45,7 @@ function automatic_GitHub_updates($data) {
             // 'package'     => $file->assets[0]->browser_download_url,
             'package'     => $file->zipball_url,
         );
-    }  
+    }
     return $data;
 }
 
