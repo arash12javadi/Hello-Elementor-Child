24398c7f797eb1b67986b60b77d2823474164c3a
Arash Javadi <arash12javadi@gmail.com>
2024-01-29 14:09:59 +0000

    Update functions.php

--- functions.php
+++ functions.php
@@ -30,7 +30,7 @@ function automatic_GitHub_updates($data) {
     $theme      = get_stylesheet(); // Folder name of the current theme
     $current    = wp_get_theme()->get('Version'); // Get the version of the current theme
     $user       = 'arash12javadi'; // The GitHub username hosting the repository
-    $repo       = 'Hello-Elementor-Child-Theme'; // Repository name as it appears in the URL
+    $repo       = 'Hello-Elementor-Child'; // Repository name as it appears in the URL
     $file       = @json_decode(@file_get_contents('https://api.github.com/repos/'.$user.'/'.$repo.'/releases/latest', false, stream_context_create(['http' => ['header' => "User-Agent: ".$user."\r\n"]])));
     $update     = filter_var($file->tag_name, FILTER_SANITIZE_NUMBER_FLOAT, FILTER_FLAG_ALLOW_FRACTION);
 
