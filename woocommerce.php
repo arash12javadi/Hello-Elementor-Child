2217ef5d213557577e42034c2842d7f6326b670d
Arash Javadi <arash12javadi@gmail.com>
2024-02-13 15:25:26 +0000

    Update woocommerce.php
    woocommerce page updated by adding actions on the top and the bottom of the page

--- woocommerce.php
+++ woocommerce.php
@@ -6,17 +6,19 @@
 
 <?php get_header(); ?>
 
-<div class="content mt-5">
-    <div class="container-fluid px-5">
-        <div class="row">
+<?php do_action('woocommerce_shop_page_top'); ?>
 
-		<div class="col-xl-2 col-lg-3 AJDWP-side-bar rounded shadow pt-4 pb-4 mb-5 mb-lg-0 ">
+<div class="content mt-5 woo-page-contents">
+    <div class="container-fluid px-5">
+        <div class="row woo-page-row">
+
+		<div class="col-xl-2 col-lg-3 AJDWP-side-bar rounded shadow pt-4 pb-4 mb-5 mb-lg-0 woo-page-sidebar">
 			<div class="sticky-top">
 				<?php dynamic_sidebar('AJDWP-Shop-sidebar'); ; ?>
 			</div>
 		</div>
 
-		<div class="col-xl-10 col-lg-9">
+		<div class="col-xl-10 col-lg-9 woo-page-main-content">
 			<section id="post-section" class="post-section">
 				<div class="container-fluid">
 					<div class="row">
@@ -29,5 +31,7 @@
 		</div>
 	</div>
 </div>
+
+<?php do_action('woocommerce_shop_page_bottom'); ?>
 
 <?php get_footer(); ?>
