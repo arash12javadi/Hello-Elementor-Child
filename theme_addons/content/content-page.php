058c61107e0e4c26263122bcf9e760ce64de05fd
Arash Javadi <arash12javadi@gmail.com>
2025-03-09 10:01:32 +0000

    Update content-page.php
    corrected to show the image text on archive and post pages.

--- theme_addons/content/content-page.php
+++ theme_addons/content/content-page.php
@@ -69,7 +69,7 @@ $options = get_option('AJDWP_theme_options');
                         $thumb_id = get_post_thumbnail_id();
                         if ($thumb_id) {
                             $alt_text = get_post_meta($thumb_id, '_wp_attachment_image_alt', true);
-                            if (!empty($alt_text) && is_archive()) {
+                            if (!empty($alt_text) && !is_single()) {
                                 echo '<p class="text-center mt-2 small">' . esc_html($alt_text) . '</p>';
                             }
                         }
