fe580ab923624825db52a3e4cc10635b1abae963
Arash Javadi <arash12javadi@gmail.com>
2025-10-08 16:46:06 +0100

    version update

--- theme_addons/ajdwp_theme_settings/Theme_updates_from_the_GitHub_repo.php
+++ theme_addons/ajdwp_theme_settings/Theme_updates_from_the_GitHub_repo.php
@@ -1,5 +1,5 @@
-<?php 
-if ( ! defined( 'ABSPATH' ) ) {
+<?php
+if (! defined('ABSPATH')) {
     exit; // Exit if accessed directly.
 }
 
@@ -10,7 +10,8 @@ if ( ! defined( 'ABSPATH' ) ) {
 // This filter checks GitHub for the latest release and, if a newer version is available,
 // informs WordPress of the update details.
 add_filter('pre_set_site_transient_update_themes', 'automatic_GitHub_theme_updates', 100, 1);
-function automatic_GitHub_theme_updates($data) {
+function automatic_GitHub_theme_updates($data)
+{
     $theme   = get_stylesheet(); // Folder name of the current theme (expected to be Hello-Elementor-Child-Theme)
     $current = wp_get_theme()->get('Version'); // Current version of the theme
     $user    = 'arash12javadi'; // GitHub username
@@ -29,14 +30,14 @@ function automatic_GitHub_theme_updates($data) {
 
     // Fetch the API response
     $response = @file_get_contents($url, false, $context);
-    if ( false === $response ) {
+    if (false === $response) {
         error_log('GitHub API request failed for URL: ' . $url);
         return $data;
     }
 
     // Decode the JSON response
     $file = json_decode($response);
-    if ( is_null($file) || empty($file->tag_name) ) {
+    if (is_null($file) || empty($file->tag_name)) {
         error_log('Invalid GitHub release data: ' . $response);
         return $data;
     }
@@ -45,7 +46,7 @@ function automatic_GitHub_theme_updates($data) {
     $update = filter_var($file->tag_name, FILTER_SANITIZE_NUMBER_FLOAT, FILTER_FLAG_ALLOW_FRACTION);
 
     // If a new version exists, set up the update data with the GitHub zipball URL
-    if ( version_compare($update, $current, '>') ) {
+    if (version_compare($update, $current, '>')) {
         $data->response[$theme] = [
             'theme'       => $theme,
             'new_version' => $update,
